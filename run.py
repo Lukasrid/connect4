e5ad71210f2b1e348a62a233bb6adb6f445cde23
Lukasrid <lukas.ridstrom@gmail.com>
2023-02-01 11:22:26 +0000

    added column numbers to game board

--- run.py
+++ run.py
@@ -4,10 +4,11 @@ board = [['⚪', '⚪', '⚪', '⚪', '⚪', '⚪', '⚪'],
          ['⚪', '⚪', '⚪', '⚪', '⚪', '⚪', '⚪'],
          ['⚪', '⚪', '⚪', '⚪', '⚪', '⚪', '⚪'],
          ['⚪', '⚪', '⚪', '⚪', '⚪', '⚪', '⚪'],
-         ['⚪', '⚪', '⚪', '⚪', '⚪', '⚪', '⚪']]
+         ['⚪', '⚪', '⚪', '⚪', '⚪', '⚪', '⚪'],
+         ['1️⃣ ', '2️⃣ ', '3️⃣ ', '4️⃣ ', '5️⃣ ', '6️⃣ ', '7️⃣']]
 
 
-ROWS = 6
+ROWS = 7
 COLUMNS = 7
 
 
@@ -20,5 +21,13 @@ def print_board():
             print(board[row][col], end=' ')
         print(" ")
 
+"""
+def placed_chip(col, player):
+    '''
+    Places chip in the first empty slot from the bottom in a column
+    '''
+    col = col - 1
+    for rows in range(ROWS-1, -1, -1):
+        if board[rows][col] == '⚪':"""
 
 print_board()
